938ac2dab21e3cae382e5ddaec0914238bad031e
saugusth <54689875+saugusth@users.noreply.github.com>
2021-04-23 18:12:10 -0500

    fixed registration page

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/com/example/finalproject/RegistrationActivity.java
@@ -0,0 +1,69 @@
+package com.example.finalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegistrationActivity extends AppCompatActivity {
+    EditText Eusername;
+    EditText Epassword;
+    Button signup;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference().child("users");
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
+        setContentView(R.layout.activity_main);
+        Eusername = findViewById(R.id.editTextUsername);
+        Epassword = findViewById(R.id.editTextPassword);
+        signup = findViewById(R.id.bsignup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = Eusername.getText().toString();
+                String password = Epassword.getText().toString();
+
+                if(username.isEmpty() && password.isEmpty()){
+                    Toast.makeText(RegistrationActivity.this, "username and password cannot be empty", Toast.LENGTH_SHORT).show();
+                }
+                else if (username.isEmpty()){
+                    Toast.makeText(RegistrationActivity.this, "username cannot be empty", Toast.LENGTH_SHORT).show();
+                }
+                else if (password.isEmpty()){
+                    Toast.makeText(RegistrationActivity.this, "password cannot be empty", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    HashMap<String, String> loginMap = new HashMap<>();
+                    loginMap.put("username", username);
+                    loginMap.put("password", password);
+                    myRef.push().setValue(loginMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(RegistrationActivity.this, "user has been added successfully", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    Intent login = new Intent(RegistrationActivity.this, MainActivity.class);
+                    startActivity(login);
+                }
+
+            }
+        });
+    }
+}
